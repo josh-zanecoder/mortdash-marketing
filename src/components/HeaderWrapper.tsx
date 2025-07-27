3f0b31d41ca353bdebacd1223c7bd7d822aac069
josh daniel sara <joshdanielsara@192.168.254.103>
2025-07-28 07:11:25 +0800

    MM- add header to the dashboard

--- src/components/HeaderWrapper.tsx
+++ src/components/HeaderWrapper.tsx
@@ -3,7 +3,5 @@ import Header from "./Header";
 import { usePathname } from "next/navigation";
 
 export default function HeaderWrapper() {
-  const pathname = usePathname();
-  if (pathname === "/dashboard") return null;
   return <Header />;
 } 
