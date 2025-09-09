566ba994cd95502034251e00538e38ff7582615f
josh daniel sara <joshdanielsara@192.168.254.110>
2025-09-09 20:52:07 +0800

    MM-56- Set a default url

--- src/config/mortdash.ts
+++ src/config/mortdash.ts
@@ -5,5 +5,5 @@ export const mortdash_ameritrust_ae_url = process.env.NEXT_PUBLIC_AMERITRUST_AE_
 export const mortdash_wsc_base_url = process.env.NEXT_PUBLIC_WSC_BASE_URL
 export const mortdash_wsc_ae_url = process.env.NEXT_PUBLIC_WSC_AE_URL
 
-export const mortdash_default_base_url = 'localhost:3000'
-export const mortdash_default_ae_url = 'localhost:3000'
+export const mortdash_default_base_url =  process.env.NEXT_PUBLIC_AMERITRUST_BASE_URL
+export const mortdash_default_ae_url = process.env.NEXT_PUBLIC_AMERITRUST_AE_URL
