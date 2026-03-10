a88f265bde1ca6503912bb2fa151e88093d00596
josh daniel sara <joshdanielsara@192.168.254.117>
2026-03-10 20:57:47 +0800

    AM-420 - change logo to convere

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -3,4 +3,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Convere Marketing",
   description: "Convere Marketing",
+  icons: {
+    icon: "/assets/convere-logo.png",
+  },
 }; 
